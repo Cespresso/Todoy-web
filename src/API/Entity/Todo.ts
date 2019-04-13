a6be7a9feb7250858abda14c121b2d540bcd95b4
Cespresso <2cespresso@gmail.com>
2019-04-13 11:54:00 +0900

    サーバー側のカラム変更に対応

--- src/API/Entity/Todo.ts
+++ src/API/Entity/Todo.ts
@@ -1,15 +1,15 @@
 export default class Todo {
     public id: number;
     public userId: string;
-    public description: string;
+    public completed: boolean;
     public title: string;
     public body: string;
 
     constructor(title: string, body: string);
-    constructor(title: string, body: string, id?: number, userId?: string, description?: string){
+    constructor(title: string, body: string, id?: number, userId?: string, completed?: boolean){
         this.id = id || null;
         this.userId = userId || null;
-        this.description = description || null;
+        this.completed = completed || false;
         this.title = title || null;
         this.body = body || null;
     }
