8c6cd7140f9ce81af1715b9cdef8ec236ca2e541
Cespresso <2cespresso@gmail.com>
2019-04-14 18:10:02 +0900

    About画面の実装

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>about</h1>
+  <v-flex xs12 sm10 offset-sm1>
+      <div id="account-info" class="text-xs-center">
+            <v-avatar size="96px">
+            <img
+                class="img-circle elevation-7 mb-1"
+                src="@/assets/logo.png"
+            >
+            </v-avatar>
+            <div class="name"> <span style="font-weight:bold">Todoy-web</span></div>
+            <div class="email text-xs-center grey--text pt-1 pb-3">v1.0.0</div>
+            <v-layout justify-space-between>
+            </v-layout>
+        </div>
+    </v-flex>
 </template>
 <script>
 export default {
