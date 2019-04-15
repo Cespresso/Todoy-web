7e053edd3ad8cf85c3c56918e98f98021469551d
Cespresso <2cespresso@gmail.com>
2019-04-16 01:16:53 +0900

    todoのcrudのテストコードの追加

--- tests/unit/API/DAO.spec.ts
+++ tests/unit/API/DAO.spec.ts
@@ -1,51 +1,109 @@
 import { expect } from 'chai';
 import DAO from '@/API/DAO';
 import Todo from '@/API/Entity/Todo'
+import "reflect-metadata";
+import {plainToClass} from "class-transformer";
 
 const Token = '';
 describe('DAO Test', () => {
-    it('DAO.instance make same instance when pass', () => {
-        const dao = DAO.getinstance();
-        expect(dao).to.deep.equal(DAO.getinstance())
+    it('Daoは複数回取得しても同じインスタンスかどうか', () => {
+        const dao = DAO.getDebugInstance();
+        expect(dao).to.deep.equal(DAO.getDebugInstance())
     });
 
-    it('axios get server status by DAO when pass', async () => {
-        const dao = DAO.getinstance();
+    it('サーバーと通信できるかどうか', async () => {
+        const dao = DAO.getDebugInstance();
         const result = await dao.getServerStatus().catch((error)=>{
             expect.fail(error);
         })
         expect(result).to.not.be.empty;
     });
 
-    it('axios get All Todos which is a specify user', async()=>{
-        const dao = DAO.getinstance();
+    it('Read ユーザーのトークンからtodoをサーバーが取得できそれがTodoのオブジェクトかどうか', async()=>{
+        const dao = DAO.getDebugInstance();
         const result = await dao.getAllTodosByAuthToken(Token).catch((error)=>{
             expect.fail(error);
         })
-        const todo = result.data[0] as Todo;
+        const todo = result.data.map((item)=>plainToClass(Todo,item))[0]
         console.log(todo)
-        if(todo instanceof Todo){
-            expect(todo).to.be.an.instanceof(Todo);
-        }
-        expect.fail('miss');
+        expect(todo).to.be.an.instanceof(Todo);
     });
-
-    it('todo CRUD test', async()=>{
-        // TODO crud テストを作成する
-
-        // create
-        const dao = DAO.getinstance();
-        const result = await dao.postTodo(Token,new Todo("vue","Test")).catch((error)=>{
+    it('Create 正しくTodoを追加して、それがサーバーから取得できるかどうか', async()=>{
+        const dao = DAO.getDebugInstance();
+        // 現在のタイムスタンプをbodyに設定する
+        let body:string =  (new Date()).getTime().toString()
+        const createResult = await dao.postTodo(Token,new Todo("vue-test-todo",body)).catch((error)=>{
             expect.fail(error);
         })
-        // read
+        console.log("todoの追加に成功しました")
+        const readResult = await dao.getAllTodosByAuthToken(Token).catch((error)=>{
+            expect.fail(error);
+        })
+        const matchLength = readResult.data.filter(todo=>todo.body === body).length
+        expect(matchLength).to.be.equal(1)
+    })
 
-        // update
+    it('Edit ', async()=>{
+        const dao = DAO.getDebugInstance();
+        // 現在のタイムスタンプをbodyに設定する
+        let body:string =  (new Date()).getTime().toString()
 
-        // read2
+        // Todo を新規作成
+        let createTodo:Todo = new Todo("vue-test-todo",body);
+        const createResult = await dao.postTodo(Token,createTodo).catch((error)=>{
+            expect.fail(error);
+        })
+        console.log("todoの追加に成功しました")
+        const readResult = await dao.getAllTodosByAuthToken(Token).catch((error)=>{
+            expect.fail(error);
+        })
+        const matchTodos = readResult.data.filter(todo=>todo.body === body)
+        expect(matchTodos.length).to.be.equal(1)
 
-        // delete
 
-        // read3
-    });
+        // タイトルを書き替える
+        matchTodos[0].title = "vue-test-todo-edited"
+        const editResult = await dao.editTodo(Token,matchTodos[0]).catch((error)=>{
+            expect.fail(error);
+        })
+        console.log("todoの書き換えに成功しました")
+        const readResult2 = await dao.getAllTodosByAuthToken(Token).catch((error)=>{
+            expect.fail(error);
+        })
+        const matchLength2 = readResult2.data.filter(todo=>todo.body === body && todo.title === "vue-test-todo-edited").length
+        expect(matchLength2).to.be.equal(1)
+
+    })
+
+    it('Delete ', async()=>{
+        const dao = DAO.getDebugInstance();
+        // 現在のタイムスタンプをbodyに設定する
+        let body:string =  (new Date()).getTime().toString()
+
+        // Todo を新規作成
+        let createTodo:Todo = new Todo("vue-test-todo",body);
+        const createResult = await dao.postTodo(Token,createTodo).catch((error)=>{
+            expect.fail(error);
+        })
+        console.log("todoの追加に成功しました")
+        const readResult = await dao.getAllTodosByAuthToken(Token).catch((error)=>{
+            expect.fail(error);
+        })
+        const matchTodos = readResult.data.filter(todo=>todo.body === body)
+        expect(matchTodos.length).to.be.equal(1)
+
+
+        // 削除
+        const editResult = await dao.deleteTodo(Token,matchTodos[0]).catch((error)=>{
+            expect.fail(error);
+        })
+        console.log("todoの削除に成功しました")
+        dao.getAllTodosByAuthToken(Token).catch((error)=>{
+            expect.fail(error);
+        }).then(()=>{
+            expect(true).to.be.true
+        })
+
+    })
+
 });
