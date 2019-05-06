ae8adcbd5fe2215f78e864c6d83159aba1baaf57
Cespresso <2cespresso@gmail.com>
2019-05-06 17:22:04 +0900

    バージョン番号の修正

--- src/const.ts
+++ src/const.ts
@@ -1,1 +1,3 @@
-
+export default class CONST {
+    static readonly version_number: string = '0.1.0';
+}
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,26 +1,23 @@
 <template>
   <v-flex xs12 sm10 offset-sm1>
-      <div id="account-info" class="text-xs-center">
-            <v-avatar size="96px">
-            <img
-                class="img-circle elevation-7 mb-1"
-                src="@/assets/logo.png"
-            >
-            </v-avatar>
-            <div class="name"> <span style="font-weight:bold">Todoy-web</span></div>
-            <div class="email text-xs-center grey--text pt-1 pb-3">v1.0.0</div>
-            <v-layout justify-space-between>
-            </v-layout>
-        </div>
-    </v-flex>
+    <div id="account-info" class="text-xs-center">
+      <v-avatar size="96px">
+        <img class="img-circle elevation-7 mb-1" src="@/assets/logo.png">
+      </v-avatar>
+      <div class="name">
+        <span style="font-weight:bold">Todoy-web</span>
+      </div>
+      <div class="email text-xs-center grey--text pt-1 pb-3">{{ version }}</div>
+      <v-layout justify-space-between></v-layout>
+    </div>
+  </v-flex>
 </template>
-<script>
-export default {
-  data: () => ({
-    drawer: null
-  }),
-  props: {
-    source: String
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+import CONST from "@/const";
+export default class About extends Vue {
+  private get version() {
+    return "v" + CONST.version_number;
   }
-};
+}
 </script>
